d117ec3c5a5be51a223bfbe234316d6623638a7f
Kaan Koyuncu <koyuncukaan@gmail.com>
2022-05-08 00:27:34 +0300

    fetch eth orders from API

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,10 +1,13 @@
-import { React, useEffect, useState } from "react";
+import { React, useEffect, useState, useCallback } from "react";
 import axios from "axios";
 
 const url = "https://api.godsunchained.com/v0/proto";
+const ethURL = "https://api.x.immutable.com/v1/orders?";
 
-function Test() {
-  const [cards, setCards] = useState([]);
+function Table() {
+  const [cards, setCards] = useState([1]);
+  const [loading, setLoading] = useState(true);
+  const [ethPrices, setEthPrices] = useState([]);
 
   const fetchTotal = async () => {
     try {
@@ -16,15 +19,51 @@ function Test() {
       );
       const cardData = response.data.records;
       // console.log(cardData);
-      setCards(cardData);
+      setCards(cardData.slice(0, 10));
+      setLoading(false);
     } catch (error) {
       console.log(error.response);
     }
   };
-
+  const fetchEthOrder = async (cardID) => {
+    try {
+      const response = await axios(ethURL, {
+        params: {
+          direction: "asc",
+          include_fees: "true",
+          order_by: "buy_quantity",
+          page_size: "1",
+          sell_metadata: `{"proto":["${cardID}"],"quality":["Meteorite"]}`,
+          sell_token_type: "ERC721",
+          status: "active",
+          sell_token_address: "0xacb3c6a43d15b907e8433077b6d38ae40936fe2c",
+          buy_token_type: "ETH",
+        },
+      });
+      const cardName = response.data.result[0].sell.data.properties.name;
+      const ethPrice =
+        response.data.result[0].buy.data.quantity / 1000000000000000000;
+      // console.log(`${cardName} ${ethPrice}`);
+      return { cardName, ethPrice };
+    } catch (error) {
+      console.log(error.response);
+    }
+  };
+  const handleClick = () => {
+    cards.map((item) => {
+      const { id } = item;
+      let jsonPromise = fetchEthOrder(id);
+      jsonPromise.then((result) =>
+        console.log(result.cardName + result.ethPrice)
+      );
+    });
+  };
   useEffect(() => {
     fetchTotal();
-  }, [url]);
+  }, []);
+  if (loading) {
+    return <h1>Loading...</h1>;
+  }
   return (
     <main className="box-border p-0 m-0 bg-slate-400 ">
       <section className="pt-20 mt-4 bg-slate-700 sm:container sm:mx-auto ">
@@ -37,8 +76,14 @@ function Test() {
           );
         })}
       </section>
+      <button
+        onClick={() => handleClick()}
+        className="px-4 py-2 font-bold text-white bg-blue-500 rounded-full hover:bg-blue-700"
+      >
+        Click Me
+      </button>
     </main>
   );
 }
 
-export default Test;
+export default Table;
